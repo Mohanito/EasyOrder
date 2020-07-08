63fb3654889571b3a9b03fb8cb133b0852a6d858
Mohan <Mohan@liyuedeMacBook.local>
2020-07-08 08:48:56 -0500

    able to read number of orders

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 def main():
     manager = Manager()
-    manager.update_sot()
-    sheet = pd.read_excel('order_list.xlsx', sheet_name = 'Sheet1')
-    print("Column headings:")
-    print(sheet.columns)
+    input_path = 'order_list.xlsx'
+    manager.set_input_path(input_path)
+    manager.read_excel()
+
 
 if __name__ == "__main__":
     main()
--- manager.py
+++ manager.py
@@ -1,5 +1,18 @@
+import pandas as pd
+
 class Manager:
     def __init__(self):
-        order_list = []
-    def update_sot(self):
-        print("hello")
+        self.input_path = ""
+        self.order_list = []
+        self.dataframe = pd.DataFrame()
+        self.num_orders = 0
+
+    def set_input_path(self, path):
+        self.input_path = path
+
+    def read_excel(self):
+        print("Reading input file...")
+        self.dataframe = pd.read_excel('order_list.xlsx', sheet_name = 'Sheet1')
+        temp_series = self.dataframe[self.dataframe.columns[0]]
+        self.num_orders = temp_series[1:].max()
+        print("# of orders: " + str(self.num_orders))
