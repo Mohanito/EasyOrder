d03176f339524316390e6b010979f0ebd68cb4e8
Mohan <Mohan@liyuedeMacBook.local>
2020-08-10 19:17:41 -0500

    8/10

--- manager.py
+++ manager.py
@@ -28,15 +28,15 @@ class Manager:
             if ".xls" in filename:
                 new_order = Order(self.input_path + '/' + filename)
                 new_order.read_input()
-                # （每个订单号的玻璃累计周长（米）/涂胶速度(input)）+ 每片玻璃直角的涂胶时间(input)*4*玻璃片数）+（等待时间(input)*玻璃片数）+（切换时间(input)*玻璃片数/35）
-                new_order.SOT = (new_order.total_circumference / self.tujiao_speed) + (self.zhijiao_tujiao * 4 * new_order.amount) + (self.wait_time + new_order.amount) + (self.switch_time * new_order.amount / 35)
+                # （每个订单号的玻璃累计周长（mm）/涂胶速度(m/min)）+ 每片玻璃直角的涂胶时间(s)*4*玻璃片数）+（等待时间(s)*玻璃片数）+（切换时间(s)*玻璃片数/35）
+                new_order.SOT = (new_order.total_circumference / self.tujiao_speed) / (1000 / 60) + (self.zhijiao_tujiao * 4 * new_order.amount) + (self.wait_time + new_order.amount) + (self.switch_time * new_order.amount / 35)
                 self.order_list.append(new_order)
 
     def output(self):
         workbook = xlwt.Workbook()
         sheet = workbook.add_sheet('Sheet1')
         # Sort using EDD
-        self.order_list.sort(key = lambda x: x.EDD, reverse = False)
+        self.sort_sot()
 
         for i in range(len(HEADINGS)):
             sheet.write(0, i, HEADINGS[i])
@@ -46,6 +46,7 @@ class Manager:
             sheet.write(i + 1, 0, i + 1)
             sheet.write(i + 1, 2, current_order.order_num)
             sheet.write(i + 1, 3, current_order.order_code)
+            sheet.write(i + 1, 5, int(current_order.thickness))
             sheet.write(i + 1, 6, current_order.amount)
             sheet.write(i + 1, 7, current_order.total_area)
             sheet.write(i + 1, 8, current_order.total_longer)
@@ -60,12 +61,13 @@ class Manager:
     def sort_edd(self):
         self.order_list.sort(key = lambda x: x.EDD, reverse = False)
         for order in self.order_list:
-            print(str(order.order_index) + ": EDD = " + str(order.EDD))
+            print(str(order.order_num) + ": EDD = " + str(order.EDD))
+        # need to update STR after sorting?
 
     def sort_sot(self):
         self.order_list.sort(key = lambda x: x.SOT, reverse = False)
         for order in self.order_list:
-            print(str(order.order_index) + ": SOT = " + str(order.SOT))
+            print(str(order.order_num) + ": SOT = " + str(order.SOT))
     
     def sort_str(self):
         pass
--- order.py
+++ order.py
@@ -14,13 +14,15 @@ class Order:
         self.total_area = 0
         self.total_circumference = 0
         self.total_longer = 0
+        self.SOT = 0
+        self.thickness = ""     # 4 5 6 8 10 12 bb 单片玻璃厚度
         # TODO:
-        self.SOT = 0 #SOT  # needs to be calculated
         self.STR = 0
     
     def read_input(self):
         self.order_num = self.worksheet.cell(3, 3).value
         self.order_code = self.worksheet.cell(7, 4).value
+        self.thickness = self.order_code[0] if self.order_code[0] != "1" else self.order_code[0:2]
         self.deadline = xlrd.xldate.xldate_as_datetime(self.worksheet.cell(9, 24).value, self.workbook.datemode)
         self.EDD = (self.deadline - datetime.datetime.now()).days
         self.amount = self.worksheet.cell(7, 32).value
