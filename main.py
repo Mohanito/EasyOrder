7c44f55f1f1fbb3f00d182dc91a7d368269d7c2f
Mohan <Mohan@liyuedeMacBook.local>
2020-08-17 18:30:50 -0500

    add units

--- main.py
+++ main.py
@@ -38,15 +38,24 @@ def main():
     label4 = tk.Label(frame, text = '输入切换时间：', bg = bg_color)
     label4.place(x = 0, y = 200)
 
-    button = tk.Button(frame, text = 'RUN', bg = 'white', command = lambda: GUI_on_click(lower_label, manager, entry1.get(), entry2.get(), entry3.get(), entry4.get()))
+    label5 = tk.Label(frame, text = '米/分钟', bg = bg_color)
+    label5.place(x = 350, y = 50)
+    label6 = tk.Label(frame, text = '秒', bg = bg_color)
+    label6.place(x = 350, y = 100)
+    label7 = tk.Label(frame, text = '秒', bg = bg_color)
+    label7.place(x = 350, y = 150)
+    label8 = tk.Label(frame, text = '秒', bg = bg_color)
+    label8.place(x = 350, y = 200)
+
+    button = tk.Button(frame, text = '运行', bg = 'white', command = lambda: GUI_on_click(lower_label, manager, entry1.get(), entry2.get(), entry3.get(), entry4.get()))
     button.place(relx = 0.45, rely = 0.9)
 
-    lower_frame = tk.Frame(frame, bg = 'white')
+    lower_frame = tk.Frame(frame)
     lower_frame.place(relx=0.5, rely=0.45, relwidth=0.9, relheight=0.4, anchor='n')
 
-    lower_label = tk.Label(lower_frame)
+    lower_label = tk.Label(lower_frame, bg = 'white')
     lower_label.place(relwidth = 1, relheight = 1)
-    lower_label['text'] = 'welcome'
+    lower_label['text'] = '欢迎使用\n请填入数据，然后点击运行'
 
     root.mainloop()
     
