e09cf536a6495fa65dc5008bf76bccfa76e689c9
Mohan <Mohan@liyuedeMacBook.local>
2020-08-19 07:37:10 -0500

    GUI radio buttons and shift length

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ def main():
     entry3.place(x = 150, y = 150)
     entry4 = tk.Entry(frame, bg = 'white', font = 32)
     entry4.place(x = 150, y = 200)
+    entry5 = tk.Entry(frame, bg = 'white', font = 32)
+    entry5.place(x = 150, y = 250)
 
     label1 = tk.Label(frame, text = '输入涂胶速度：', bg = bg_color)
     label1.place(x = 0, y = 50)
@@ -47,11 +49,23 @@ def main():
     label8 = tk.Label(frame, text = '秒', bg = bg_color)
     label8.place(x = 350, y = 200)
 
+    label9 = tk.Label(frame, text = '标准作业时间：', bg = bg_color)
+    label9.place(x = 0, y = 250)
+    label10 = tk.Label(frame, text = '小时', bg = bg_color)
+    label10.place(x = 350, y = 250)
+
+    rb_var = tk.IntVar()    # num_shift
+    
     button = tk.Button(frame, text = '运行', bg = 'white', command = lambda: GUI_on_click(lower_label, manager, entry1.get(), entry2.get(), entry3.get(), entry4.get()))
     button.place(relx = 0.45, rely = 0.9)
 
+    rb1 = tk.Radiobutton(frame, text = '单班作业制', variable = rb_var, value = 1, command = lambda: rb_on_click(manager, rb_var))
+    rb1.place(x = 400, y = 200)
+    rb2 = tk.Radiobutton(frame, text = '双班作业制', variable = rb_var, value = 2, command = lambda: rb_on_click(manager, rb_var))
+    rb2.place(x = 400, y = 250)
+
     lower_frame = tk.Frame(frame)
-    lower_frame.place(relx=0.5, rely=0.45, relwidth=0.9, relheight=0.4, anchor='n')
+    lower_frame.place(relx=0.5, rely=0.5, relwidth=0.9, relheight=0.35, anchor='n')
 
     lower_label = tk.Label(lower_frame, bg = 'white')
     lower_label.place(relwidth = 1, relheight = 1)
@@ -70,13 +84,19 @@ def GUI_on_click(lower_label, manager, tujiao_speed, zhijiao_tujiao, wait_time, 
         manager.zhijiao_tujiao = float(zhijiao_tujiao)
         manager.wait_time = float(wait_time)
         manager.switch_time = float(switch_time)
-        lower_label['text'] = 'data type correct'
+        lower_label['text'] = '数据符合要求'
         try:
             manager.read_input_dir()    # orders get duplicated on click
             manager.output()
-            lower_label['text'] += '\ndone, please check result.xls'
+            lower_label['text'] += '\n已完成, 输出表格为result.xls'
+            lower_label['text'] += '\n请关闭窗口，勿再次点击运行按钮'
         except:
             lower_label['text'] = 'Unexpected error'    # tested, works
+
+def rb_on_click(manager, rb_var):
+    # 单/双班工作制
+    print(rb_var.get())
+    manager.num_shift = rb_var.get()
 
 
 if __name__ == "__main__":
